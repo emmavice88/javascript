bcc6397d4eae68cd70e49f2f6a6672d90bb67d2b
Vicencio Emmanuel <emmavice160388@gmail.com>
2023-07-08 20:38:30 -0300

    iniciandoproyecto

---
+++ index.js
@@ -0,0 +1,19 @@
+// esti es comentario
+
+
+
+/*
+
+
+
+todo lo que este entre estos dos se comenta
+sino se puede seleccionar y  con control //
+
+
+
+*/
+
+
+const pi = 31
+let nombre = "daniel"
+
